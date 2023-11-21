bff061fd591a3db97ae15c2059bd71cc7d98b35b
Tengo Gogisvanidze <tengo6531@gmail.com>
2023-11-21 21:50:43 +0400

    ...

---
+++ modules/hello_youtube.py
@@ -0,0 +1,10 @@
+import messages as msg
+# from messages import hello,by # *
+
+# hello()
+# by()
+
+# msg.hello()
+# msg.by()
+
+help('modules')
---
+++ modules/messages.py
@@ -0,0 +1,5 @@
+def hello():
+    print("Hello! Have a nice day!")
+
+def by():
+    print("Bye! Have a wonderful time")
